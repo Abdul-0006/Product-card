2094ca71e9a7c4b08e148cedd211f0f0dfbc3399
Abdul-0006 <154364633+Abdul-0006@users.noreply.github.com>
2025-09-05 16:20:14 +0500

    Changes

--- src/Components/footer.jsx
+++ src/Components/footer.jsx
@@ -3,7 +3,7 @@ import './card.css'
 const Footer = () => {
   return (
     <footer className="footer">
-      <p>Check out Kababjee Bakers <a href="#bakers">here</a></p>
+      <p>Check out Kababjee Bakers <a href="https://share.google/SqSQqgaXqRBVpOfac">here</a></p>
       <p className="copyright">&copy; 2024 Kababjees. All rights reserved.</p>
     </footer>
   )
